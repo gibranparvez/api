6593712372db92805786532e071afb9cda6c6e37
Julian Simioni <julian@simioni.org>
2018-06-12 16:29:11 -0400

    test2

--- .circleci/docker.sh
+++ .circleci/docker.sh
@@ -6,14 +6,14 @@ DATE=`date +%Y-%m-%d`
 DOCKER_REPOSITORY="pelias"
 DOCKER_PROJECT="${DOCKER_REPOSITORY}/${CIRCLE_PROJECT_REPONAME}"
 
-BRANCH="${CIRCLE_BRANCH/\//-}" #slashes are not valid in docker tags. replace with dashes
+BRANCH="$(echo $CIRCLE_BRANCH | tr '/' '-')" #slashes are not valid in docker tags. replace with dashes
 
 # the name of the image that represents the "branch", that is an image that will be updated over time with the git branch
 # the production branch is changed to "latest", otherwise the git branch becomes the name of the version
-if [[ "${CIRCLE_BRANCH}" == "production" ]]; then
+if [[ "${BRANCH}" == "production" ]]; then
 	DOCKER_BRANCH_IMAGE_VERSION="latest"
 else
-	DOCKER_BRANCH_IMAGE_VERSION="${BRANCH/\//-}" #slashes are not valid in docker tags. replace with dashes
+	DOCKER_BRANCH_IMAGE_VERSION="${BRANCH}" #slashes are not valid in docker tags. replace with dashes
 fi
 DOCKER_BRANCH_IMAGE_NAME="${DOCKER_PROJECT}:${DOCKER_BRANCH_IMAGE_VERSION}"
 
